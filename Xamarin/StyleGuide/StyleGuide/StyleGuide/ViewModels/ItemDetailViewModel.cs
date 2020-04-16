414349f57978342daab568433b189037adde882b
boguslawblonski <bblonski@o2.pl>
2020-04-16 12:05:49 +0200

    init commit

---
+++ Xamarin/StyleGuide/StyleGuide/StyleGuide/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using StyleGuide.Models;
+
+namespace StyleGuide.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Xamarin/StyleGuide/StyledApp.Less/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require("gulp"),
+    fs = require("fs"),
+    less = require("gulp-less");
+
+
+gulp.task("Build Less File", function () {
+    return gulp.src('MainPage.less')
+        .pipe(less())
+        .pipe(gulp.dest('../StyleGuide/Styles'));
+});
+
+
+
+gulp.task('default', ['Build Less File']); 
